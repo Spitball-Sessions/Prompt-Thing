5805441d2746b659e6b4ad660fd015b0af5b128e
Joshua Noyes <Koholos@yahoo.com>
2024-12-01 17:02:20 -0500

    clicking on prompt card displays text in \'display\' area.  Added a couple of test prompts for now.  need to resize \'display\' area.

--- prompt.js
+++ prompt.js
@@ -1,1 +1,30 @@
-prompt
+
+function getElements(){
+    const leftSide = document.getElementById("leftSidebar")
+    const posQueue = document.getElementById("positiveQueue");
+    const negQueue = document.getElementById("negativeQueue");
+    const promptBoxes = document.getElementById("promptBoxes");
+
+    return{posQueue,negQueue,promptBoxes,leftSide}
+};
+
+
+const q = getElements();
+
+q.leftSide.addEventListener("dblclick", (event) =>{
+    const card = event.target.closest(".card")
+    if (card){
+        let text = card.querySelector(".promptText").textContent;
+        const display = promptBoxes.querySelector("#promptDisplay"); 
+        text = text.replace('<br>','\n')
+        console.log(text)
+        display.textContent = text;
+
+
+    }
+    else{
+        return
+    }
+}) 
+
+
