468df71e2ff0973dfa3be4786031584a62f3a739
Joshua Noyes <Koholos@yahoo.com>
2024-12-01 20:57:44 -0500

    update

--- prompt.js
+++ prompt.js
@@ -3,9 +3,11 @@ function getElements(){
     const leftSide = document.getElementById("leftSidebar")
     const posQueue = document.getElementById("positiveQueue");
     const negQueue = document.getElementById("negativeQueue");
-    const promptBoxes = document.getElementById("promptBoxes");
+    const promptInputs = document.getElementById("promptBoxes");
+    const promptDisplay = document.getElementById("upperSection");
 
-    return{posQueue,negQueue,promptBoxes,leftSide}
+
+    return{posQueue,negQueue,promptInputs,leftSide,promptDisplay}
 };
 
 
@@ -15,12 +17,10 @@ q.leftSide.addEventListener("dblclick", (event) =>{
     const card = event.target.closest(".card")
     if (card){
         let text = card.querySelector(".promptText").textContent;
-        const display = promptBoxes.querySelector("#promptDisplay"); 
+        const display = q.promptDisplay.querySelector("#promptDisplay"); 
         text = text.replace('<br>','\n')
         console.log(text)
         display.textContent = text;
-
-
     }
     else{
         return
