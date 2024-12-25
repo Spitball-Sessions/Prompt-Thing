4491cb977876c0b55b96997ed3ca954cb424f4c3
Joshua Noyes <Koholos@yahoo.com>
2024-12-25 18:07:17 -0500

    consolidated create methods and added more test info

--- prompt.js
+++ prompt.js
@@ -13,21 +13,26 @@ function englishSetter(input){
 
 //gets HTML elements.  probably more than needed.
 function getElements(){
-    const leftSide = document.getElementById("leftSidebar")
+    const queueContainer = document.getElementById("leftSidebar");
     const posQueue = document.getElementById("positiveQueue");
     const negQueue = document.getElementById("negativeQueue");
     const promptInputs = document.getElementById("promptBoxes");
     const promptDisplay = document.getElementById("upperSection");
-    const savePos = document.getElementById("savePositive");
-    const saveNeg = document.getElementById("saveNegative");
-    return{posQueue,negQueue,promptInputs,leftSide,promptDisplay,saveNeg,savePos}
+    const savePositivePrompt = document.getElementById("savePositive");
+    const saveNegativePrompt = document.getElementById("saveNegative");
+    return{posQueue,negQueue,promptInputs,queueContainer,promptDisplay,saveNegativePrompt,savePositivePrompt}
 };
+//immediately call.
+const {posQueue,negQueue,promptInputs,queueContainer,promptDisplay,saveNegativePrompt,savePositivePrompt} = getElements();
 
-function createArrays(){
+
+//creates arrays for Positive and Negative prompts.
+const {posPrompts,negPrompts} = (function (){
     const posPrompts = [];
     const negPrompts = [];
     return {posPrompts,negPrompts}
-}
+})();
+
 
 class Prompt {
     constructor(name,promptText,type,description=""){
@@ -37,26 +42,29 @@ class Prompt {
         this.type = type;
     }
 
-    set promptText(input){
-        this._promptText = englishSetter(input).concat("",",");
-        }
+    set promptText(input){this._promptText = englishSetter(input).concat("",",");}
+    set description(input){this._description = englishSetter(input);}
 
-    set description(input){
-        this._description = englishSetter(input);
-    }
-
-    get promptText(){
-        return this._promptText;
-    }
-
-    get description(){
-        return this._description;
-    }
+    get promptText(){return this._promptText;}
+    get description(){return this._description;}
 }
 
+//temporary
+const prompt1 = new Prompt("posprompt1", "girl, intricate, detailed face, high res, photorealistic,upper body, close up, from above, looking up, night, back turned, looking back, pitch black, dark alleyway, graffiti wall, 1girl, backless short black dress, playful, allure, sensation, sultry, pouting, (masterpiece, high quality, best quality)","positive","Back Alley");
+const prompt2 = new Prompt("posprompt2","Aesthethic: {2.5D flat-color anime art. draped in shadows with a ring lighting to enhances her unique features.}/nForeground: {A woman with a bright and wispy ethereal mist surrounding her body. The mist creates a dramatic and surreal effect. The girl is wearing an intricate and tattered gown. The bottom of the gown is covered in blood. The woman's face has a blank and cold expression, her eyes are covered by her long blue hair. Holding finger to lips, shush, shushing motion}/nBackground: {A neon-lit night cityscape filled with a swarm of flying doves. The city has large futuristic buildings and strong contrast between light and shadows. There is a hint of danger and intrigue.}","positive","cyberpunk");
+posPrompts.push(prompt1,prompt2)
+db(posPrompts[1].name)
+prompt1.name = "negprompt1";
+prompt1.promptText = "low quality, medium quality, blurry, censored, blurry, censored, wrinkles, deformed,lowres, worst quality, normal quality, text, watermark, mutated, Bad art, mutation, deformed iris, deformed pupils, bad limbs, conjoined, featureless, messed up clothes, bad features, incorrect background, incorrect objects, cropped out face, messed up accessories, text in image, piercings, logo";
+prompt1.type = "negative";
+prompt1.description = "Basic Negative Prompt";
+negPrompts.push(prompt1);
+db(negPrompts[0].promptText)
 
-function createNewPrompt(child, prefix){
-    let newPosition = 0
+//convert to "create cards from prompts"
+function createNewPrompt(prompt, child){
+    let newPosition = 0;
+    let prefix = prompt.type;
     positionNumber = String(child).slice(-1);
     /*since all the HTML id's are shorthanded to 'pos' and 'neg', 
     this will append the correct queue based on the function entered from */
@@ -68,9 +76,13 @@ function createNewPrompt(child, prefix){
     else{//if no cards exist, last child will be header, so last letter of the tag will be 
         newPosition = 1
     }
-    
+
+    idname = shortPrefix + "Card" + newPosition;
+    prompt.name = idname
+    db(prompt)
+
     const newCard = document.createElement("div");
-    newCard.setAttribute("id",shortPrefix+ "Card" + newPosition);
+    newCard.setAttribute("id",idname);
     newCard.classList.add(prefix, "card");
 
     const input = document.createElement("input");
@@ -89,21 +101,14 @@ function createNewPrompt(child, prefix){
     p.classList.add("promptText");
     newCard.appendChild(p);
     if(shortPrefix == "neg"){
-        q.negQueue.appendChild(newCard);
+        negQueue.appendChild(newCard);
     }
     else{
-        q.posQueue.appendChild(newCard);
+        posQueue.appendChild(newCard);
     }
-
 
     return p
 }
-
-//store prompts in arrays
-
-
-const q = getElements();
-const {posPrompts,negPrompts} = createArrays();
 
 function copyPromptCardToDisplay(event){
     const card = event.target.closest(".card")
@@ -119,27 +124,7 @@ function copyPromptCardToDisplay(event){
     }
 }
 
-function savePositivePrompt(event){
-    const input = document.getElementById("promptInput")
-    const lastCard = q.posQueue.lastElementChild.id;
-    const prompt = new Prompt(undefined,input.value,"positive","")
-    paragraph = createNewPrompt(lastCard,prompt.type); 
-    paragraph.textContent = prompt.promptText;
-    posPrompts.push(prompt);
-    db(posPrompts);
-    input.value = "";
-} 
 
-function saveNegativePrompt(event){
-    const input = document.getElementById("promptInput")
-    const lastCard = q.negQueue.lastElementChild.id;
-    const prompt = new Prompt(undefined,input.value,"negative","")
-    paragraph = createNewPrompt(lastCard,prompt.type);
-    paragraph.textContent = prompt.promptText;
-    negPrompts.push(prompt);
-    db(negPrompts);
-    input.value = "";
-}
 
 function labelPromptCards(event){
     if(event.key === "Enter"){
@@ -154,10 +139,25 @@ function labelPromptCards(event){
     }
 }
 
-q.leftSide.addEventListener("dblclick", copyPromptCardToDisplay);
+//takes queue assigned based on event button.  creates new prompt and adds to positive or negative queue array.
+function savePrompt(type){
+    const input = document.getElementById("promptInput");
+    const prompt = new Prompt(undefined,input.value,type,"");
+    if (type === "positive"){
+        posPrompts.push(prompt);
+        db(posPrompts);
+    } 
+    else{
+        negPrompts.push(prompt);
+        db(negPrompts);
+    }
+    input.value = "";
+}
+
+queueContainer.addEventListener("dblclick", copyPromptCardToDisplay);
 
 //saving prompts
-q.savePos.addEventListener("click", savePositivePrompt);
-q.saveNeg.addEventListener("click", saveNegativePrompt);
+savePositivePrompt.addEventListener("click", ()=> savePrompt("positive"));
+saveNegativePrompt.addEventListener("click", ()=> savePrompt("negative"));
 
-q.leftSide.addEventListener("keypress",labelPromptCards);
+queueContainer.addEventListener("keypress",labelPromptCards);
